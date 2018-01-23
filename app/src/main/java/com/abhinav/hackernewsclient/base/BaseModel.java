e85da7e9de8d37cf02d24171cc488d0b1285a098
abhinav272 <abhinav.ih@gmail.com>
2018-01-23 14:55:22 +0530

    BaseModel created on reactive pattern

---
+++ app/src/main/java/com/abhinav/hackernewsclient/base/BaseModel.java
@@ -0,0 +1,79 @@
+package com.abhinav.hackernewsclient.base;
+
+import com.abhinav.hackernewsclient.network.FailureResponse;
+
+import java.lang.ref.SoftReference;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import retrofit2.HttpException;
+
+/**
+ * Created by appinventiv on 23/1/18.
+ */
+
+abstract class BaseModel<T extends BaseModelListener> implements Observer<T> {
+
+    private SoftReference<T> listener;
+    private CompositeDisposable disposable;
+
+    public BaseModel(T listener) {
+        attachListener(listener);
+    }
+
+    public void attachListener(T listener) {
+        this.listener = new SoftReference<>(listener);
+        disposable = new CompositeDisposable();
+    }
+
+    public void detachListener() {
+        this.listener = null;
+        flushDisposable();
+    }
+
+    public T getListener() {
+        return (listener != null) ? listener.get() : null;
+    }
+
+    @Override
+    public void onError(Throwable e) {
+        if (e != null && (e instanceof SocketTimeoutException || e instanceof UnknownHostException)) {
+            noNetworkAvailableError();
+        } else {
+            onSpecificErrorOccurred(e);
+        }
+    }
+
+    @Override
+    public void onSubscribe(Disposable d) {
+        disposable.add(d);
+    }
+
+    @Override
+    public void onComplete() {
+        disposable.clear();
+    }
+
+    private void flushDisposable() {
+        if (!disposable.isDisposed()) {
+            disposable.dispose();
+        }
+    }
+
+    private void onSpecificErrorOccurred(Throwable e) {
+        FailureResponse failureResponse = null;
+        if (e != null && e instanceof HttpException) {
+            failureResponse = new FailureResponse();
+            failureResponse.setErrorCode(((HttpException) e).code());
+            failureResponse.setMsg(((HttpException) e).message());
+        }
+        getListener().onErrorOccured(failureResponse);
+    }
+
+    private void noNetworkAvailableError() {
+        getListener().noNetworkError();
+    }
+}

7b9a6a3492a0951bd3b36a74a5a7739088c21ce3
abhinav272 <abhinav.ih@gmail.com>
2018-01-23 13:40:50 +0530

    FailureResponse class created to handle all failure cases

---
+++ app/src/main/java/com/abhinav/hackernewsclient/network/FailureResponse.java
@@ -0,0 +1,26 @@
+package com.abhinav.hackernewsclient.network;
+
+/**
+ * Created by appinventiv on 23/1/18.
+ */
+
+public class FailureResponse {
+    private int errorCode;
+    private String msg;
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}

58228368b8296a96adc19a9f02610308ceda7cdc
abhinav272 <abhinav.ih@gmail.com>
2018-01-25 10:17:42 +0530

    DataManager refactor to fetch topstories using rx

--- app/src/main/java/com/abhinav/hackernewsclient/data/DataManager.java
+++ app/src/main/java/com/abhinav/hackernewsclient/data/DataManager.java
@@ -1,18 +1,22 @@
 package com.abhinav.hackernewsclient.data;
 
+import com.abhinav.hackernewsclient.data.network.pojo.Story;
+
 import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.functions.Function;
 
 /**
  * DataManager is the single source of truth
  * RestController is so written to be only accessible by DataManger only
  * If offline support is to be integrated, a similar class for DBController can be written
  * which is only accessible by DataManager.
- *
+ * <p>
  * All Components only seek data from DataManager whereas DataManager decides if it needs to
  * fetch it from local or Server.
- *
+ * <p>
  * DataManger is so written to facilitate easy custom cache implementation as well as populating
  * old local data to User while fetching fresh data from Server and refresh user screen.
  */
@@ -21,15 +25,31 @@ public class DataManager {
     private static final DataManager ourInstance = new DataManager();
     private RestController restController;
 
-    public static DataManager getInstance() {
-        return ourInstance;
-    }
-
     private DataManager() {
         restController = RestController.getInstance();
     }
 
-    public Observable<List<Long>> fetchTopStories() {
-        return restController.getTopStories();
+    public static DataManager getInstance() {
+        return ourInstance;
+    }
+
+    public Observable<Story> fetchTopStories() {
+        return restController.getTopStories()
+                .concatMap(new Function<List<Long>, ObservableSource<? extends Story>>() {
+                    @Override
+                    public ObservableSource<? extends Story> apply(List<Long> longs) throws Exception {
+                        return getStoryByIds(longs);
+                    }
+                });
+    }
+
+    private ObservableSource<Story> getStoryByIds(List<Long> storyIds) {
+        return Observable.fromIterable(storyIds)
+                .concatMap(new Function<Long, ObservableSource<Story>>() {
+                    @Override
+                    public ObservableSource<Story> apply(Long aLong) throws Exception {
+                        return restController.getStoryDetails(aLong);
+                    }
+                });
     }
 }

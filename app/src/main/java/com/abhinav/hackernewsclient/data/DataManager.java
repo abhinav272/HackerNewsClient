f161941aaf6e8fe6219bb928d5608c585ea2ed6b
abhinav272 <abhinav.ih@gmail.com>
2018-01-27 20:47:00 +0530

    datamanger methods created for fetching comments and reply

--- app/src/main/java/com/abhinav/hackernewsclient/data/DataManager.java
+++ app/src/main/java/com/abhinav/hackernewsclient/data/DataManager.java
@@ -1,5 +1,6 @@
 package com.abhinav.hackernewsclient.data;
 
+import com.abhinav.hackernewsclient.data.network.pojo.Comment;
 import com.abhinav.hackernewsclient.data.network.pojo.Story;
 
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.List;
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
 
 /**
  * DataManager is the single source of truth
@@ -49,6 +51,75 @@ public class DataManager {
                     @Override
                     public ObservableSource<Story> apply(Long aLong) throws Exception {
                         return restController.getStoryDetails(aLong);
+                    }
+                });
+    }
+/*
+    public Observable<Comment> fetchStoryComments(List<Long> storyKids, final int depthLevelToAchieve) {
+        return Observable.fromIterable(storyKids)
+                .concatMap(new Function<Long, ObservableSource<Comment>>() {
+                    @Override
+                    public ObservableSource<Comment> apply(Long aLong) throws Exception {
+                        return restController.getCommentDeatils(aLong);
+                    }
+                })
+                .concatMap(new Function<Comment, ObservableSource<? extends Comment>>() {
+                    @Override
+                    public ObservableSource<? extends Comment> apply(Comment comment) throws Exception {
+                        comment.setDepthLevel(comment.getDepthLevel() + 1);
+                        if (comment.getDepthLevel() == depthLevelToAchieve || comment.getKids() == null
+                                || comment.getKids().size() == 0 || comment.isDeleted())
+                            return Observable.just(comment);
+                        else
+                            return Observable.just(comment)
+                            .mergeWith(fetchStoryComments(comment.getKids(), depthLevelToAchieve - comment.getDepthLevel()));
+                    }
+                })
+                .filter(new Predicate<Comment>() {
+                    @Override
+                    public boolean test(Comment comment) throws Exception {
+                        return comment != null && comment.getKids().size() > 0 &&
+                                !comment.isDeleted() && comment.getText() != null;
+                    }
+                });
+    }*/
+
+    public Observable<Comment> fetchStoryComments(List<Long> storyKids) {
+        return Observable.fromIterable(storyKids)
+                .concatMap(new Function<Long, ObservableSource<? extends Comment>>() {
+                    @Override
+                    public ObservableSource<? extends Comment> apply(Long aLong) throws Exception {
+                        return restController.getCommentDeatils(aLong);
+                    }
+                })
+                .filter(new Predicate<Comment>() {
+                    @Override
+                    public boolean test(Comment comment) throws Exception {
+//                        return !comment.isDeleted() && comment.getKids() != null && comment.getKids().size() >= 0;
+                        return !comment.isDeleted();
+                    }
+                });
+    }
+
+    public Observable<Comment> fetchCommentsReply(List<Long> commentKidIds) {
+        return Observable.fromIterable(commentKidIds)
+                .concatMap(new Function<Long, ObservableSource<? extends Comment>>() {
+                    @Override
+                    public ObservableSource<? extends Comment> apply(Long aLong) throws Exception {
+                        return restController.getCommentDeatils(aLong);
+                    }
+                })
+                .flatMap(new Function<Comment, ObservableSource<Comment>>() {
+                    @Override
+                    public ObservableSource<Comment> apply(Comment comment) throws Exception {
+                        comment.setDepthLevel(1);
+                        return Observable.just(comment);
+                    }
+                })
+                .filter(new Predicate<Comment>() {
+                    @Override
+                    public boolean test(Comment comment) throws Exception {
+                        return !comment.isDeleted();
                     }
                 });
     }

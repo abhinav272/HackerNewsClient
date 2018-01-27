f822387a567809c614f9067ca41df50aac8a30a0
abhinav272 <abhinav.ih@gmail.com>
2018-01-27 20:48:04 +0530

    comment presenter functionality added

--- app/src/main/java/com/abhinav/hackernewsclient/ui/comments/CommentsPresenter.java
+++ app/src/main/java/com/abhinav/hackernewsclient/ui/comments/CommentsPresenter.java
@@ -1,8 +1,61 @@
 package com.abhinav.hackernewsclient.ui.comments;
 
+import com.abhinav.hackernewsclient.base.BasePresenter;
+import com.abhinav.hackernewsclient.data.network.pojo.Comment;
+
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * Created by appinventiv on 26/1/18.
  */
 
-public class CommentsPresenter {
+public class CommentsPresenter extends BasePresenter<CommentsView> implements CommentsModelListener {
+
+    private CommentsModel model;
+    private Queue<List<Long>> commentReplies = new ArrayDeque<>();
+
+    public CommentsPresenter(CommentsView view) {
+        super(view);
+        model = new CommentsModel(this);
+    }
+
+    @Override
+    public void onCommentsLoaded(Comment comment) {
+        getView().showComment(comment);
+        if (comment.getKids() != null && comment.getKids().size() > 0)
+            commentReplies.add(comment.getKids());
+    }
+
+    @Override
+    public void onCommentsLoadComplete() {
+        model.fetchCommentsReply(commentReplies.poll());
+    }
+
+    @Override
+    public void onCommentReplyLoaded(Comment comment) {
+        getView().showCommentsReply(comment);
+    }
+
+    @Override
+    protected void setModel() {
+
+    }
+
+    @Override
+    protected void destroy() {
+        model.detachListener();
+        commentReplies = null;
+        model = null;
+    }
+
+    protected void initFetching(List<Long> commentIds) {
+        model.fetchStoryComments(commentIds);
+    }
+
+    @Override
+    protected void initView() {
+
+    }
 }
